51cba5bae4ccb2522ad90a7e433f3b2b1758c91d
cally91 <cally91@naver.com>
2019-07-26 16:50:22 +0900

    홈페이지연습

--- project/src/main/java/com/biz/vigun/dao/MemberDao.java
+++
@@ -1,9 +0,0 @@
-package com.biz.memo.dao;
-
-import com.biz.memo.model.MemberVO;
-
-public interface MemberDao {
-
-	public MemberVO findByUserId(String m_userid);
-	public MemberVO login(MemberVO memberVO);
-}
